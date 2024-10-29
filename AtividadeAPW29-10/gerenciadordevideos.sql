8d68db1bbd37b380bbcfb9ebbe2a02540f50fe24
Yago Pereira <yagopereiranunes6@gmail.com>
2024-10-29 11:16:35 -0300

    Update gerenciadordevideos.sql

--- AtividadeAPW29-10/gerenciadordevideos.sql
+++ AtividadeAPW29-10/gerenciadordevideos.sql
@@ -3,6 +3,12 @@ use gerenciadordevideos;
 
 drop schema gerenciadordevideos;
 
-create table if not exists videos (id int not null auto_increment primary key, nome varchar(255), link varchar(255));
+create table if not exists videos (id int not null auto_increment primary key, nome varchar(255), link varchar(255), embed varchar(255));
 
 select * from videos;
+
+insert into videos (nome, link, embed) values ('Lo Fi Girl', 'https://www.youtube.com/watch?v=0r5U34iB7CU', 'https://www.youtube.com/embed/0r5U34iB7CU');
+insert into videos (nome, link, embed) values ('Potholderz', 'https://www.youtube.com/watch?v=oYLHXItjzG4', 'https://www.youtube.com/embed/oYLHXItjzG4');
+
+
+
